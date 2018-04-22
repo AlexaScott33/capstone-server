f93354bf4ca37d4ae689a65d025f2cd9cd8d5ad8
Alexa Scott <arscott4792@gmail.com>
2018-04-21 18:28:07 -0700

    added get by id endpoint

--- router/matches.js
+++ router/matches.js
@@ -19,4 +19,27 @@ router.get('/matches', (req, res) => {
     });
 });
 
+/* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/matches/:id', (req, res) => {
+  const { id } = req.params;
+
+  /***** Never trust users - validate input *****/
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    console.error(err);
+  }
+
+  Match.findById(id)
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+
+
 module.exports = router;
